49d39bb974e4b98927e9df3d9451fad17998ffc2
Alex Bellamy <alexjbellamy@gmail.com>
2022-11-28 17:29:33 -0500

    more deployment stuff

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,17 +7,17 @@ const Navigation = () => {
             <div className="navbar-menu is-active">
                 <div className="navbar-start">
                     <div className="navbar-item">
-                    <NavLink to="/" className="navbar-item">
+                    <NavLink to="/react-portfolio/" className="navbar-item">
                         <h3>About Me</h3>
                     </NavLink>
                     </div>
-                    <NavLink to="/portfolio" className="navbar-item">
+                    <NavLink to="/react-portfolio/portfolio" className="navbar-item">
                         <h3>Portfolio</h3>
                     </NavLink>
-                    <NavLink to="/contact" className="navbar-item">
+                    <NavLink to="/react-portfolio/contact" className="navbar-item">
                         <h3>Contact</h3>
                     </NavLink>
-                    <NavLink to="/resume" className="navbar-item">
+                    <NavLink to="/react-portfolio/resume" className="navbar-item">
                         <h3>Resume</h3>
                     </NavLink>
                 </div>
